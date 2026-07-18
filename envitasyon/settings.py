f3c2cd842ca9ff6adcf369507e675e9a42cbaf7c
Mario Michel <mariomichel58@gmail.com>
2026-07-17 18:50:43 -0600

    changes in settings

--- envitasyon/settings.py
+++ envitasyon/settings.py
@@ -96,8 +96,9 @@ DATABASES = {
     }
 }"""
 
+
 # Database — Railway PostgreSQL
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('PGDATABASE'),
@@ -107,7 +108,7 @@ DATABASES = {
         'PORT': os.environ.get('PGPORT'),
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
